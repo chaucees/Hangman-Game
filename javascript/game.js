fd55dd8ee21b60be29827dcb93549841aece29df
Chaucee <hi@chaucee.com>
2017-11-18 18:57:49 -0700

    Sat Night

--- javascript/game.js
+++ javascript/game.js
@@ -2,27 +2,72 @@
 
 var wins = "";
 var currentScore = 0;
-var winningScore = 0;
-var randomWordArr = ["fishtown", "south silly", "ben franklin", "cheesesteak"];
-var randomWord = randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
+var randomWords = ["fishtown", "pretzel", "libertybell", "cheesesteak"];
+var randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+var currentState = [ ];
 
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// This creates the underscores when the page loads
+for (var i = 0; i < randomWord.length; i++) {
+    currentState.push(" _ ");
+}
+
+// USER GUESS 
+document.onkeyup = function(event) {
+    
+    var userGuess = event.key;
+
+    // DISPLAYS USER GUESS
+    document.getElementById("user-guess").innerHTML = userGuess; 
+
+    for (var i = 0; i < randomWord.length; i++) {
+        if (userGuess === randomWord[i]) {
+            currentState[i] = userGuess;
+        }
+    }
+    document.getElementById("current-word").innerHTML = currentState.join(" ");
+
+    // // PRINT GUESSED LETTERS - doesn't work
+    // var wrongGuess= [];
+
+    // for (var i = 0; i < randomWord.length; i++) {
+    //     if (userGuess === randomWord[i]) {
+    //         currentState[i] = wrongGuess;
+    //     }
+    // }
+
+    // document.getElementById("wrong-guess").innerHTML = wrongGuess; 
+    
+    // }
+
+// If they guess the whole word, reset the current state to blank underscores with next word.
+if (userGuess === randomWords[i]) {
+    currentstore+1;
+}
+// WIN + LOOSE LOGIC HERE INSIDE THIS
+
+// Loop the game until the user guesses all four words
+
+// Update the score +1 each time they finish completing a word
+
+var currentScore = 0;
+
+for (var i = 0; i < randomWords.legnth; i++) {
+    if (userGuess === randomWords[i]) {
+        currentScore[i++];
+    }
+}
+
+function update() {
+    currentScore.text = "SCORE: " + currentScore;}
+
+// Displays Current Score
+
+    document.getElementById("wins").innerHTML =currentScore;}
+
+// NUMBERS OF GUESSES REMAINING 
+
+// RESET BUTTON
 
 
-// Conditional Statements
 
-// You Win Display
-
-// 
-
-
-
-
-
-//Clear Button
-$("#clear").on("click", function() {
-    $("#display").empty();
-});    
 
